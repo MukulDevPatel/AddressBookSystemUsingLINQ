d07bdaf6f390f633e36c7866d52e81a7c2db207d
MukulDevPatel <patelmukuldev@gmail.com>
2023-07-05 08:45:02 +0530

    [Mukul Dev] Add: We create address book attributes for the inserting contacts

--- AddressBookSystemUsingLINQ/Program.cs
+++ AddressBookSystemUsingLINQ/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Address Book System Using LINQ");
+
+        Person person = new Person()
+        {
+            FirstName = "Dev",
+            LastName = "Patel",
+            Address = "Vidya nagar",
+            City = "Mumbai",
+            State = "Maharashtra",
+            Zip = 456231,
+            PhoneNumber = 6524545863,
+            Email = "devpatel@gmail.com"
+        };
+        Console.WriteLine(person.FirstName+"\n"+ person.LastName + "\n" + person.Address+ "\n" + person.City+ "\n" +
+            person.State+ "\n" + person.Zip+ "\n" + person.PhoneNumber+ "\n" + person.Email);
     }
 } 
